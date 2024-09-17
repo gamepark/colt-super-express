acef416c3bc7750a23c0e6fb9b8467fabdcc5f05
Bruno Mahiet <devwebmb@gmail.com>
2024-09-17 16:01:31 +0200

    debut BanditFigureLocator

--- app/src/locators/BanditFigureLocator.ts
+++ app/src/locators/BanditFigureLocator.ts
@@ -1,14 +1,27 @@
-import { Locator, MaterialContext } from "@gamepark/react-game";
+import {  Locator, MaterialContext } from "@gamepark/react-game";
 import { Location } from "@gamepark/rules-api";
+import { trainCardDescription } from "../material/TrainCardDescription";
 
 class BanditFigureLocator extends Locator {
-  getCoordinates(_location: Location, _context:MaterialContext) {
 
-      return {
-          y:6
+  getCoordinates(_location: Location, context: MaterialContext) {
+    const firstPlayer = context.rules.game.items[4]?.[0].id;
+    const players = context.rules.game.players;
+    console.log(firstPlayer);
+    console.log(context.rules.game.players);
+    console.log(context.rules.game.items[3]);
+    console.log(context.player);
+    
+    
+
+    for (let i = 1; i <= players.length; i++) {
+      if (players[i] === firstPlayer) {
+        return { y: 0, x: 0 - trainCardDescription.width };
       }
+    }
 
+    return {};
   }
 }
 
-export const banditFigureLocator = new BanditFigureLocator()
+export const banditFigureLocator = new BanditFigureLocator();
--- app/src/locators/FirstPlayerCardLocator.ts
+++ app/src/locators/FirstPlayerCardLocator.ts
@@ -1,17 +1,84 @@
-import {
-  Locator,
-  MaterialContext,
-} from "@gamepark/react-game";
+import { Locator, MaterialContext } from "@gamepark/react-game";
 import { Location } from "@gamepark/rules-api";
 
 class FirstPlayerLocator extends Locator {
   getCoordinates(_location: Location, context: MaterialContext) {
+    const firstPlayer = context.rules.game.items[4]?.[0].id;
+    const playersLenght = context.rules.game.players.length;
+    const players = context.rules.game.players;
 
-console.log(context.material);
+    switch (playersLenght) {
+      case 2:
+        if (context.player === firstPlayer) {
+          return { y: 10 };
+        } else {
+          return { y: -10 };
+        }
 
-
-
-    return { y: -2 };
+      case 3:
+        if (context.player === firstPlayer) {
+          return { y: 10 };
+        } else if (players[1] === firstPlayer) {
+          return { y: -10, x: -20 };
+        } else {
+          return { y: -10, x: 20 };
+        }
+      default:
+        break;
+      case 4:
+        if (context.player === firstPlayer) {
+          return { y: 10, x: -20 };
+        } else if (players[1] === firstPlayer) {
+          return { y: -10, x: -20 };
+        } else if (players[2] === firstPlayer) {
+          return { y: -10, x: 20 };
+        } else {
+          return { y: 10, x: 20 };
+        }
+      case 5:
+        if (context.player === firstPlayer) {
+          return { y: 10, x: -15 };
+        } else if (players[1] === firstPlayer) {
+          return { y: -10, x: -25 };
+        } else if (players[2] === firstPlayer) {
+          return { y: -10, x: 0 };
+        } else if (players[3] === firstPlayer) {
+          return { y: -10, x: 25 };
+        } else {
+          return { y: 10, x: 15 };
+        }
+      case 6:
+        if (context.player === firstPlayer) {
+          return { y: 10, x: 0 };
+        } else if (players[1] === firstPlayer) {
+          return { y: 10, x: -25 };
+        } else if (players[2] === firstPlayer) {
+          return { y: -10, x: -25 };
+        } else if (players[3] === firstPlayer) {
+          return { y: -10, x: 0 };
+        } else if (players[4] === firstPlayer) {
+          return { y: -10, x: 25 };
+        } else {
+          return { y: 10, x: 25 };
+        }
+      case 7:
+        if (context.player === firstPlayer) {
+          return { y: 10, x: 0 };
+        } else if (players[1] === firstPlayer) {
+          return { y: 10, x: -25 };
+        } else if (players[2] === firstPlayer) {
+          return { y: -10, x: -36 };
+        } else if (players[3] === firstPlayer) {
+          return { y: -10, x: -12};
+        } else if (players[4] === firstPlayer) {
+          return { y: -10, x: 12 };
+        } else if (players[5] === firstPlayer) {
+          return { y: -10, x: 36 };
+        } else {
+          return { y: 10, x: 25 };
+        }
+    }
+    return {};
   }
 }
 
